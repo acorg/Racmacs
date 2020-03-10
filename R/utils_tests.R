e652e3620d11885a9671b27e0efc4ad62401567f
Sam Wilks <sw463@cam.ac.uk>
2020-03-10 15:13:22 +1100

    Add function to export a viewer test

--- R/utils_tests.R
+++ R/utils_tests.R
@@ -36,3 +36,29 @@ run.maptests <- function(expr, bothclasses = FALSE, loadlocally = FALSE){
 
 }
 
+
+export.viewer.test <- function(widget, filename){
+
+  maptype <- get0("maptype", parent.frame())
+  if(!is.null(maptype)){ filename <- gsub("\\.html$", paste0("_", maptype, ".html"), filename) }
+  testfile <- file.path("~/Dropbox/LabBook/packages/Racmacs/tests/testoutput", filename)
+
+  htmlwidgets::saveWidget(
+    widget,
+    file          = testfile,
+    selfcontained = FALSE,
+    libdir        = ".lib"
+  )
+
+  plotdata <- readLines(testfile)
+  plotdata <- gsub(
+    pattern     = ".lib/RacViewer-1.0.0/",
+    replacement = "../../inst/htmlwidgets/RacViewer/lib/",
+    x           = plotdata,
+    fixed       = TRUE
+  )
+  writeLines(plotdata, testfile)
+
+}
+
+

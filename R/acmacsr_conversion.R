1216bf2c5fa4e80ea9552cac447e2d4a8c0b6755
Sam Wilks <sw463@cam.ac.uk>
2020-12-01 10:12:27 +0100

    next steps

---
+++ R/acmacsr_conversion.R
@@ -0,0 +1,31 @@
+
+racchart.new <- function(chart){
+
+  # The racchart is fundamentally an environment
+  acchart <- new.env(parent = emptyenv())
+  class(acchart) <- c("racchart", "rac", "environment")
+  acchart$chart <- chart
+
+  # Return the chart
+  acchart
+
+}
+
+
+#' @rdname convertingMaps
+#' @family {functions for working with map data}
+#' @export
+as.cpp <- function(map){
+
+  if("racchart" %in% class(map)) return(map)
+  json <- as.json(map)
+  tmp <- tempfile(fileext = ".ace")
+  write(json, tmp)
+
+  chart <- suppressMessages({ new(acmacs.r::acmacs.Chart, tmp) })
+  map   <- racchart.new(chart = chart)
+
+  selectedOptimization(map.cpp) <- selectedOptimization(map)
+  map.cpp
+
+}

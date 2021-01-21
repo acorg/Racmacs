bb1d799b305b1f37fabd86bd0d284d1da4bfc4cd
Sam Wilks <sw463@cam.ac.uk>
2021-01-21 21:39:49 +0100

    Redefine rapidjsonr assertions to pass R checks

--- src/json_assert.h
+++ src/json_assert.h
@@ -3,9 +3,8 @@
 #define Racmacs__json_assert__h
 
 // Define the rapid json assert macro
-void R_assert(bool x){
-  if(!x) Rf_error("Parsing failed");
-}
-#define RAPIDJSON_ASSERT(x) R_assert(x)
+void ac_assert(bool x);
+#define RAPIDJSON_ASSERT(x) ac_assert(x)
 
 #endif
+

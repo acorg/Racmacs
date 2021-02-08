dd97c2315aeb431787284b381b753c38f5de6193
harper357 <harper357@gmail.com>
2021-02-08 21:17:49 +0000

    Added initial tests for unconstrained points.

---
+++ tests/testthat/test_underconstrained_positions.R
@@ -0,0 +1,55 @@
+
+library(testthat)
+library(Racmacs)
+context("Test underconstrained positions")
+
+# Read titer_table with missing data
+
+titertable <- read.titerTable("../testdata/titer_tables/titer_table5_underconstrained.csv")
+map <- acmap(titer_table = titertable)
+
+test_that("Warn of undercontrained positions", {
+  res <- evaluate_promise(
+    optimizeMap(
+      map = map,
+      number_of_dimensions = 2,
+      number_of_optimizations = 1,
+      minimum_column_basis = "none",
+    )
+  )
+
+  expect_warning(
+    res$warnings,
+    "Some points are undercontrained for the given dimension"
+  )
+})
+
+test_that("Optimize points if underconstrained but have finite positions", {
+  res <- evaluate_promise(
+    optimizeMap(
+      map = map,
+      number_of_dimensions = 2,
+      number_of_optimizations = 1,
+      minimum_column_basis = "none",
+    )
+  )
+  expect_false(anyNA(agBaseCoords(res$result, 1)))
+})
+
+test_that("Error for undercontrained points with infinite positions", {
+  titerTable(map)[4, 2] <- "*"
+
+  res <- evaluate_promise(
+    optimizeMap(
+      map = map,
+      number_of_dimensions = 2,
+      number_of_optimizations = 1,
+      minimum_column_basis = "none",
+    )
+  )
+
+  expect_error(
+    res$output,
+    "Some points are undercontrained for the given dimension"
+  )
+})

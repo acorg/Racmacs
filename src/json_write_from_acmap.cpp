21bea2224149451d63ce44122e4a665bc833c537
Sam Wilks <sw463@cam.ac.uk>
2021-02-18 16:15:15 +0100

    Simplify and consolidate json writing code

--- src/json_write_from_acmap.cpp
+++ src/json_write_from_acmap.cpp
@@ -1,167 +1,11 @@
-
 // #include <cstdio>
 #include <string.h>
 #include <RcppArmadillo.h>
 #include "acmap_map.h"
 #include "acmap_point.h"
 #include "acmap_optimization.h"
-
-#include "json_assert.h"
-// [[Rcpp::depends(rapidjsonr)]]
-#include <rapidjson/document.h>
-#include <rapidjson/stringbuffer.h>
-#include <rapidjson/writer.h>
-// #include <rapidjson/filereadstream.h>
+#include "json_write_from_acmap.h"
 using namespace rapidjson;
-
-// Create a double value
-Value doubleval(
-    const double& d
-){
-
-  Value val;
-  if(std::isfinite(d)){
-    val.SetDouble(d);
-  } else {
-    val.SetNull();
-  }
-  return val;
-
-}
-
-// Create a string value
-Value strval(
-  const std::string& s,
-  Document::AllocatorType& allocator
-){
-
-  Value val;
-  val.SetString( StringRef(s.c_str()), allocator );
-  return val;
-
-}
-
-// Create a string array
-Value str_vec_to_json(
-  std::vector<std::string> stringvec,
-  Document::AllocatorType& allocator
-){
-
-  Value strvecjson(kArrayType);
-  for(SizeType i=0; i<stringvec.size(); i++){
-    strvecjson.PushBack(
-      strval( stringvec[i], allocator ),
-      allocator
-    );
-  }
-  return strvecjson;
-
-}
-
-// Create a titer table record
-Value titer_table_to_json(
-  AcTiterTable& titertable,
-  Document::AllocatorType& allocator
-){
-
-  Value agrows(kArrayType);
-  for(SizeType ag=0; ag<titertable.nags(); ag++){
-    Value srtiters(kObjectType);
-    for(SizeType sr=0; sr<titertable.nsr(); sr++){
-      if(titertable.titer_measured(ag, sr)){
-        srtiters.AddMember(
-          strval( std::to_string(sr), allocator ),
-          strval(titertable.get_titer_string(ag, sr), allocator),
-          allocator
-        );
-      }
-    }
-    agrows.PushBack(srtiters, allocator);
-  }
-  return agrows;
-
-}
-
-// Convert an arma::vec to a json array
-template <typename T>
-Value vec_to_json_array(
-  const T& x,
-  Document::AllocatorType& allocator
-){
-
-  Value a(kArrayType);
-  for( arma::uword i=0; i<x.n_elem; i++ ){
-    if(std::isfinite(x(i))){
-      a.PushBack(x(i), allocator);
-    } else {
-      Value null(kNullType);
-      a.PushBack(null, allocator);
-    }
-  }
-  return a;
-
-}
-
-// Convert an arma::mat to a json array
-Value mat_to_json_array(
-  const arma::mat& m,
-  Document::AllocatorType& allocator
-){
-
-  Value a(kArrayType);
-  for( arma::uword i=0; i<m.n_rows; i++ ){
-    arma::vec row = arma::vectorise( m.row(i) );
-    a.PushBack(
-      vec_to_json_array(row, allocator),
-      allocator
-    );
-  }
-  return a;
-
-}
-
-// Create a point style record
-template <typename T>
-Value json_point_style(
-  const T& pt,
-  Document::AllocatorType& allocator
-){
-
-  Value ptstyle(kObjectType);
-  ptstyle.AddMember("+", pt.plotspec.get_shown(), allocator);
-  ptstyle.AddMember("F", strval(pt.plotspec.get_fill(), allocator), allocator);
-  ptstyle.AddMember("O", strval(pt.plotspec.get_outline(), allocator), allocator);
-  ptstyle.AddMember("o", pt.plotspec.get_outline_width(), allocator);
-  ptstyle.AddMember("S", strval(pt.plotspec.get_shape(), allocator), allocator);
-  ptstyle.AddMember("s", pt.plotspec.get_size(), allocator);
-  ptstyle.AddMember("r", pt.plotspec.get_rotation(), allocator);
-  ptstyle.AddMember("a", pt.plotspec.get_aspect(), allocator);
-  return ptstyle;
-
-}
-
-// Create a point diagnostics record
-Value pt_diagnostics_to_json(
-    const AcDiagnostics& d,
-    Document::AllocatorType& allocator
-){
-
-  Value ptdiag(kObjectType);
-
-  // Hemisphering info
-  Value pthemi(kArrayType);
-  for(arma::uword i=0; i<d.hemi.size(); i++){
-    Value hemi(kObjectType);
-    hemi.AddMember("d", strval(d.hemi[i].diagnosis, allocator), allocator);
-    hemi.AddMember("c", vec_to_json_array(d.hemi[i].coords, allocator), allocator);
-    pthemi.PushBack(hemi, allocator);
-  }
-  ptdiag.AddMember("h", pthemi, allocator);
-
-  return ptdiag;
-
-}
-
 
 // [[Rcpp::export]]
 std::string acmap_to_json(
@@ -176,7 +20,7 @@ std::string acmap_to_json(
 
   // Add basic info
   doc.AddMember("_", "-*- js-indent-level: 2 -*-", allocator);       // json info..?
-  doc.AddMember("  version", strval(version, allocator), allocator); // Version info
+  doc.AddMember("  version", jsonifya(version, allocator), allocator); // Version info
   doc.AddMember("?created", "", allocator);                          // Comment field
 
   // Map information
@@ -198,8 +42,8 @@ std::string acmap_to_json(
     AcAntigen& ag = map.antigens[i];
     Value agval(kObjectType);
 
-    agval.AddMember("N", strval(ag.get_name(), allocator), allocator);
-    agval.AddMember("P", strval(ag.get_passage(), allocator), allocator);
+    agval.AddMember("N", jsonifya(ag.get_name(), allocator), allocator);
+    agval.AddMember("P", jsonifya(ag.get_passage(), allocator), allocator);
     // set_group_values
     // set_date
     // set_reference
@@ -220,8 +64,8 @@ std::string acmap_to_json(
     AcSerum& sr = map.sera[i];
     Value srval(kObjectType);
 
-    srval.AddMember("N", strval(sr.get_name(), allocator), allocator);
-    srval.AddMember("P", strval(sr.get_passage(), allocator), allocator);
+    srval.AddMember("N", jsonifya(sr.get_name(), allocator), allocator);
+    srval.AddMember("P", jsonifya(sr.get_passage(), allocator), allocator);
     // set_group_values
     // set_date
     // set_reference
@@ -237,13 +81,13 @@ std::string acmap_to_json(
 
   // == TITERS ==========================
   Value t(kObjectType);
-  Value d = titer_table_to_json( map.titer_table_flat, allocator );
+  Value d = jsonifya( map.titer_table_flat, allocator );
   t.AddMember("d", d, allocator);
 
   if(map.titer_table_layers.size() > 1){
     Value L(kArrayType);
     for(arma::uword layer=0; layer<map.titer_table_layers.size(); layer++){
-      Value titertableval = titer_table_to_json( map.titer_table_layers[layer], allocator );
+      Value titertableval = jsonifya( map.titer_table_layers[layer], allocator );
       L.PushBack(titertableval, allocator);
     }
     t.AddMember("L", L, allocator);
@@ -252,13 +96,13 @@ std::string acmap_to_json(
   // == PLOTSPEC =====================
   Value p(kObjectType);
   Value ptstyles(kArrayType);
-  arma::ivec ptstyle_indices( num_points );
+  arma::uvec ptstyle_indices( num_points );
 
   // Antigens
   for(int i=0; i<num_antigens; i++){
 
     // Generate the point style
-    Value ptstyle = json_point_style(map.antigens[i], allocator);
+    Value ptstyle = jsonifya(map.antigens[i].plotspec, allocator);
 
     // Check if that point style already exists
     int ptstyle_index = -1;
@@ -284,7 +128,7 @@ std::string acmap_to_json(
   for(int i=0; i<num_sera; i++){
 
     // Generate the point style
-    Value ptstyle = json_point_style(map.sera[i], allocator);
+    Value ptstyle = jsonifya(map.sera[i].plotspec, allocator);
 
     // Check if that point style already exists
     int ptstyle_index = -1;
@@ -307,11 +151,11 @@ std::string acmap_to_json(
   }
 
   // Add to the plotspec json
-  p.AddMember("p", vec_to_json_array(ptstyle_indices, allocator), allocator);
+  p.AddMember("p", jsonifya(ptstyle_indices, allocator), allocator);
   p.AddMember("P", ptstyles, allocator);
 
   // Drawing order
-  p.AddMember("d", vec_to_json_array(map.get_pt_drawing_order(), allocator), allocator);
+  p.AddMember("d", jsonifya(map.get_pt_drawing_order(), allocator), allocator);
 
 
   // == OPTIMIZATION RUNS ======================
@@ -323,24 +167,24 @@ std::string acmap_to_json(
     Value optjson(kObjectType);
 
     // Comment
-    optjson.AddMember("c", strval(map.optimizations[i].get_comment(), allocator), allocator);
+    optjson.AddMember("c", jsonifya(map.optimizations[i].get_comment(), allocator), allocator);
 
     // Stress
-    optjson.AddMember("s", doubleval(map.optimizations[i].stress), allocator);
+    optjson.AddMember("s", jsonify(map.optimizations[i].stress), allocator);
 
     // Minimum column basis
-    optjson.AddMember("m", strval(map.optimizations[i].get_min_column_basis(), allocator), allocator);
+    optjson.AddMember("m", jsonifya(map.optimizations[i].get_min_column_basis(), allocator), allocator);
 
     // Fixed column bases
-    optjson.AddMember("C", vec_to_json_array( map.optimizations[i].get_fixed_column_bases(), allocator), allocator);
+    optjson.AddMember("C", jsonifya( map.optimizations[i].get_fixed_column_bases(), allocator), allocator);
 
     // Transformation
     arma::vec transformation_vec = arma::vectorise( map.optimizations[i].get_transformation() );
-    optjson.AddMember("t", vec_to_json_array( transformation_vec , allocator ), allocator);
+    optjson.AddMember("t", jsonifya( transformation_vec , allocator ), allocator);
 
     // Coords
     arma::mat coords = arma::join_cols( map.optimizations[i].get_ag_base_coords(), map.optimizations[i].get_sr_base_coords() );
-    optjson.AddMember("l", mat_to_json_array( coords, allocator ), allocator);
+    optjson.AddMember("l", jsonifya( coords, allocator ), allocator);
 
     // Add to array
     P.PushBack(optjson, allocator);
@@ -355,8 +199,8 @@ std::string acmap_to_json(
   for(arma::uword i=0; i<map.antigens.size(); i++){
     Value agx(kObjectType);
     agx.AddMember("g", map.antigens[i].get_group(), allocator);
-    agx.AddMember("q", strval(map.antigens[i].get_sequence(), allocator), allocator);
-    agx.AddMember("i", strval(map.antigens[i].get_id(), allocator), allocator);
+    agx.AddMember("q", jsonifya(map.antigens[i].get_sequence(), allocator), allocator);
+    agx.AddMember("i", jsonifya(map.antigens[i].get_id(), allocator), allocator);
     xa.PushBack(agx, allocator);
   }
   x.AddMember("a", xa, allocator);
@@ -366,8 +210,8 @@ std::string acmap_to_json(
   for(arma::uword i=0; i<map.sera.size(); i++){
     Value srx(kObjectType);
     srx.AddMember("g", map.sera[i].get_group(), allocator);
-    srx.AddMember("q", strval(map.sera[i].get_sequence(), allocator), allocator);
-    srx.AddMember("i", strval(map.sera[i].get_id(), allocator), allocator);
+    srx.AddMember("q", jsonifya(map.sera[i].get_sequence(), allocator), allocator);
+    srx.AddMember("i", jsonifya(map.sera[i].get_id(), allocator), allocator);
     xs.PushBack(srx, allocator);
   }
   x.AddMember("s", xs, allocator);
@@ -379,13 +223,15 @@ std::string acmap_to_json(
     Value optx(kObjectType);
 
     // Translation
-    optx.AddMember("t", vec_to_json_array(map.optimizations[i].get_translation(), allocator), allocator);
+    optx.AddMember("t", jsonifya(map.optimizations[i].get_translation(), allocator), allocator);
+
+    // Diagnostics
+    Value optxd(kArrayType);
 
     // AG Diagnostics
-    Value optxagd(kArrayType);
     for(arma::uword ag=0; ag<map.optimizations[i].ag_diagnostics.size(); ag++){
-      optxagd.PushBack(
-        pt_diagnostics_to_json(
+      optxd.PushBack(
+        jsonifya(
           map.optimizations[i].ag_diagnostics[ag],
           allocator
         ),
@@ -394,27 +240,25 @@ std::string acmap_to_json(
     }
 
     // SR Diagnostics
-    Value optxsrd(kArrayType);
-    for(arma::uword ag=0; ag<map.optimizations[i].ag_diagnostics.size(); ag++){
-      optxagd.PushBack(
-        pt_diagnostics_to_json(
-          map.optimizations[i].ag_diagnostics[ag],
+    for(arma::uword sr=0; sr<map.optimizations[i].sr_diagnostics.size(); sr++){
+      optxd.PushBack(
+        jsonifya(
+          map.optimizations[i].sr_diagnostics[sr],
           allocator
         ),
         allocator
       );
     }
 
-    optx.AddMember("ad", optxagd, allocator);
-    optx.AddMember("sd", optxsrd, allocator);
+    optx.AddMember("d", optxd, allocator);
     xp.PushBack(optx, allocator);
 
   }
   x.AddMember("p", xp, allocator);
 
   // = OTHER =
-  x.AddMember("agv", str_vec_to_json(map.get_ag_group_levels(), allocator), allocator);
-  x.AddMember("srv", str_vec_to_json(map.get_sr_group_levels(), allocator), allocator);
+  x.AddMember("agv", jsonifya(map.get_ag_group_levels(), allocator), allocator);
+  x.AddMember("srv", jsonifya(map.get_sr_group_levels(), allocator), allocator);
 
   // == FINISH UP ===============================
   // Assemble the json map data and add it

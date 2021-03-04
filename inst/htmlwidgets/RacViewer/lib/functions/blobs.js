9b5bd5420747ad779e2e416d6bed3c02b66bf488
Sam Wilks <sw463@cam.ac.uk>
2021-03-04 21:12:51 +0000

    Remove unnecessary code

--- inst/htmlwidgets/RacViewer/lib/functions/blobs.js
+++ inst/htmlwidgets/RacViewer/lib/functions/blobs.js
@@ -40,15 +40,11 @@ Racmacs.StressblobsPanel = class StressblobsPanel {
         // Create holder div
         this.div = document.createElement("div");
         this.div.classList.add("stressblobs-tab");
-
-        var stressBlobsTable = new Racmacs.StressBlobsTable(viewer);
-        this.div.appendChild(stressBlobsTable.div);
         
         var newStressBlobsWell = new Racmacs.utils.InputWell({
             inputs: [
                 { id: "stresslim",   value: 1,      label : "Stress limit" },
-                { id: "gridspacing", value: 0.25,   label : "Grid spacing" },
-                { id: "gridmargin",  value: 4,      label : "Grid margin"  }
+                { id: "gridspacing", value: 0.25,   label : "Grid spacing" }
             ],
             submit: "Calculate stress blobs",
             fn : function(args){ viewer.onAddStressBlobs(args) }
@@ -61,70 +57,12 @@ Racmacs.StressblobsPanel = class StressblobsPanel {
 
 }
 
-
-
-Racmacs.StressBlobsTable = class StressBlobsList extends Racmacs.TableList {
-
-    constructor(viewer){
-
-        // Generate skeleton
-        super({
-            title   : "Stress blobs",
-            checkbox_check_fn : function(){},
-            checkbox_uncheck_fn : function(){
-                viewer.removeBlobs();
-            },
-            headers : ["Stress lim", "Grid spacing", "Antigens", "Sera"]
-        });
-
-        // Bind to viewer
-        viewer.blobTable = this;
-        this.viewer = viewer;
-        
-    }
-
-    addData(blobs){
-
-        var stressBlobsTable = this;
-        this.addRow({
-            content : [blobs.stress_lim, blobs.grid_spacing, blobs.antigens, blobs.sera],
-            fn : function(){
-                stressBlobsTable.viewer.showBlobs(blobs.blob_data);
-            }
-        });
-
-    }
-
-}
-
 // Show blobs in the viewer
 Racmacs.Viewer.prototype.addStressBlobs = function(blobdata){
 
     // Show the blobs
-    for(var i=0; i<this.antigens.length; i++){
-        if(blobdata.antigens[i] 
-           && Object.keys(blobdata.antigens[i]).length !== 0){
-
-            this.antigens[i].addBlob(blobdata.antigens[i]);
-
-        } else {
-
-            this.antigens[i].removeBlob();
-
-        }
-    }
-    
-    for(var i=0; i<this.sera.length; i++){
-        if(blobdata.sera[i] 
-           && Object.keys(blobdata.sera[i]).length !== 0){
-
-            this.sera[i].addBlob(blobdata.sera[i]);
-
-        } else {
-
-            this.sera[i].removeBlob();
-
-        }
+    for(var i=0; i<this.points.length; i++){
+        this.points[i].addBlob(blobdata[i]);
     }
 
 }
@@ -132,17 +70,8 @@ Racmacs.Viewer.prototype.addStressBlobs = function(blobdata){
 // Remove blobs from viewer
 Racmacs.Viewer.prototype.removeBlobs = function(){
 
-    if(this.blobTable){
-        this.blobTable.checkbox.uncheck(false);
-        this.blobTable.checkbox.disable();
-    }
-        
     for(var i=0; i<this.points.length; i++){
         this.points[i].removeBlob();
-    }
-
-    if(this.blobTable){
-        this.blobTable.deselectAll();
     }
 
 }

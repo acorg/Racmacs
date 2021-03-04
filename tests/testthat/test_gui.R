0c252b8a262f331dd279238dbaa5cbda8281cd08
Sam Wilks <sw463@cam.ac.uk>
2021-03-04 21:23:32 +0000

    Link gui tests

--- tests/testthat/test_gui.R
+++ tests/testthat/test_gui.R
@@ -1,27 +1,3 @@
 
-library(testthat)
-library(Racmacs)
-context("Test GUI")
-
-warning("Need GUI tests")
-
-# invisible(lapply(list.files("R", full.names = T), source))
-#
-# # Test a null map
-# # export.viewer.test(
-# #   RacViewer(map = NULL),
-# #   filename = "null_map.html"
-# # )
-#
-# # map <- read.acmap("~/Desktop/h3map2004.ace")
-# # runGUI()
-# # mapGUI(map)
-#
-# runGUI <- function(){
-#   shiny::runApp("inst/shinyapps/RacmacsGUI/")
-# }
-#
-# runGUI()
-
-# shinytest::recordTest("inst/shinyapps/RacmacsGUI/")
-
+# This simply sources the tests in the shiny app folder
+source(system.file(package = "Racmacs", "shinyapps/RacmacsGUI/tests/testthat/test-server.R"))

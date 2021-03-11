bdead01354359021a3bf2badb9bcb67c42fe71be
Sam Wilks <sw463@cam.ac.uk>
2021-03-11 22:03:17 +0000

    Add more extensive noisy bootstrap tests

--- tests/testthat/test-diagnostics_noisyBootstrap.R
+++ tests/testthat/test-diagnostics_noisyBootstrap.R
@@ -4,7 +4,7 @@ library(testthat)
 context("Bootstrapping maps")
 
 # Set variables
-num_bs_repeats <- 10
+num_bs_repeats <- 100
 
 test_that("Test map bootstrapping", {
 
@@ -12,12 +12,44 @@ test_that("Test map bootstrapping", {
   bsmap <- bootstrapMap(
     map = map,
     bootstrap_repeats        = num_bs_repeats,
-    optimizations_per_repeat = 40,
+    optimizations_per_repeat = 10,
     ag_noise_sd              = 0.7,
     titer_noise_sd           = 0.7
   )
 
-  expect_equal(length(bsmap$bootstrap), num_bs_repeats)
+  # Save and load bootstrap data
+  tmp <- tempfile(fileext = ".ace")
+  save.acmap(bsmap, tmp)
+  bsmap <- read.acmap(tmp)
+  expect_equal(length(bsmap$optimizations[[1]]$bootstrap), num_bs_repeats)
+
+  # Calculating bootstrap blobs
+  bsmap <- bootstrapBlobs(
+    bsmap, 0.68
+  )
+
+  # Viewing bootstrap blobs
+  export.viewer.test(
+    view(bsmap),
+    "map2d_with_bootstrap_blobs.html"
+  )
+
+  # Plotting bootstrap blobs
+  export.plot.test(
+    plot(bsmap),
+    "map2d_with_bootstrap_blobs.pdf"
+  )
+
+  # Viewing 3d bootstrap blobs
+  bsmap3d <- read.acmap(test_path("../testdata/testmap_h3subset3d_1000bootstraps.ace"))
+  bsmap3d <- bootstrapBlobs(
+    bsmap3d, 0.68
+  )
+
+  export.viewer.test(
+    view(bsmap3d),
+    "map3d_with_bootstrap_blobs.html"
+  )
 
 })
 

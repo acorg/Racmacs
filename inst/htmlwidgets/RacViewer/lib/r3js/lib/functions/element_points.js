a9079cf48bd42ea4937aa6f0f5c65a3793370e4f
Sam Wilks <sw463@cam.ac.uk>
2021-03-15 20:09:49 +0000

    Hack a temporary change for allowing sphere outlines

--- inst/htmlwidgets/RacViewer/lib/r3js/lib/functions/element_points.js
+++ inst/htmlwidgets/RacViewer/lib/r3js/lib/functions/element_points.js
@@ -78,9 +78,19 @@ R3JS.element.Point = class Point extends R3JS.element.base {
       if(args.properties.outlinecolor){
         args.properties.color = args.properties.outlinecolor;
         var mat = R3JS.Material(args.properties);
-        var geo = R3JS.Geometries[args.shape].outline(this.lwd);
-        this.outline = new THREE.Mesh(geo, mat);
-        this.outline.element = this;
+        if (args.shape == "circle3d" 
+            && args.properties.fillcolor.r == 1
+            && args.properties.fillcolor.g == 1
+            && args.properties.fillcolor.b == 1) {
+          var geo = R3JS.Geometries[args.shape].outline(this.lwd);
+          this.outline = new THREE.LineSegments(geo);
+          this.outline.material.linewidth = 10;
+          this.outline.element = this;
+        } else {
+          var geo = new THREE.BufferGeometry();
+          this.outline = new THREE.Mesh(geo, mat);
+          this.outline.element = this;
+        }
         this.object.add(this.outline);
       }
 
@@ -149,7 +159,8 @@ R3JS.Geometries.circle3d = {
     return( new THREE.SphereBufferGeometry(0.1, 25, 25) );
   },
   outline : function(lwd){
-    return( new THREE.BufferGeometry() );
+    var geometry = new THREE.SphereBufferGeometry(0.1, 6, 4);
+    return( new THREE.WireframeGeometry( geometry ) );
   }
 }
 

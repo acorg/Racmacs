9ce25644d6c840f791bb88b6a431f48d0df3c5ab
Sam Wilks <sw463@cam.ac.uk>
2021-06-07 22:30:10 +0100

    Accept only .ace as input

--- inst/shinyapps/RacmacsGUI/ui.R
+++ inst/shinyapps/RacmacsGUI/ui.R
@@ -4,10 +4,10 @@ ui <- shiny::fillPage(
   shiny::includeScript("www/racmacs.js"),
   RacViewerOutput("racViewer"),
   div(
-    shiny::tags$input(id = "mapDataLoaded",        name = "mapDataLoaded",        type = "file", accept = ".ace,.save,.acd1,.acd1.bz2"),
+    shiny::fileInput(inputId = "mapDataLoaded", label = "mapDataLoaded", accept = ".ace"),
     shiny::tags$input(id = "tableDataLoaded",      name = "tableDataLoaded",      type = "file", accept = ".csv,.txt"),
-    shiny::tags$input(id = "procrustesDataLoaded", name = "procrustesDataLoaded", type = "file", accept = ".ace,.save,.acd1,.acd1.bz2"),
-    shiny::tags$input(id = "pointStyleDataLoaded", name = "pointStyleDataLoaded", type = "file", accept = ".ace,.save,.acd1,.acd1.bz2"),
+    shiny::tags$input(id = "procrustesDataLoaded", name = "procrustesDataLoaded", type = "file", accept = ".ace"),
+    shiny::tags$input(id = "pointStyleDataLoaded", name = "pointStyleDataLoaded", type = "file", accept = ".ace"),
     shinyFiles::shinySaveButton(id = "mapDataSaved",    label = "", title = "", filename = "acmap",  list(ace = ".ace")),
     shinyFiles::shinySaveButton(id = "tableDataSaved",  label = "", title = "", filename = "table",  list(csv = ".csv")),
     shinyFiles::shinySaveButton(id = "coordsDataSaved", label = "", title = "", filename = "coords", list(csv = ".csv")),

9f0d59cc1b3ccc92ce7e107cbda16b656317ddd2
Sam Wilks <sw463@cam.ac.uk>
2021-06-09 15:40:37 +0100

    Use fileInput for GUI

--- inst/shinyapps/RacmacsGUI/ui.R
+++ inst/shinyapps/RacmacsGUI/ui.R
@@ -5,9 +5,9 @@ ui <- shiny::fillPage(
   RacViewerOutput("racViewer"),
   div(
     shiny::fileInput(inputId = "mapDataLoaded", label = "mapDataLoaded", accept = ".ace"),
-    shiny::tags$input(id = "tableDataLoaded",      name = "tableDataLoaded",      type = "file", accept = ".csv,.txt"),
-    shiny::tags$input(id = "procrustesDataLoaded", name = "procrustesDataLoaded", type = "file", accept = ".ace"),
-    shiny::tags$input(id = "pointStyleDataLoaded", name = "pointStyleDataLoaded", type = "file", accept = ".ace"),
+    shiny::fileInput(inputId = "tableDataLoaded", label = "tableDataLoaded", accept = ".csv,.txt"),
+    shiny::fileInput(inputId = "procrustesDataLoaded", label = "procrustesDataLoaded", accept = ".ace"),
+    shiny::fileInput(inputId = "pointStyleDataLoaded", label = "pointStyleDataLoaded", accept = ".ace"),
     shinyFiles::shinySaveButton(id = "mapDataSaved",    label = "", title = "", filename = "acmap",  list(ace = ".ace")),
     shinyFiles::shinySaveButton(id = "tableDataSaved",  label = "", title = "", filename = "table",  list(csv = ".csv")),
     shinyFiles::shinySaveButton(id = "coordsDataSaved", label = "", title = "", filename = "coords", list(csv = ".csv")),

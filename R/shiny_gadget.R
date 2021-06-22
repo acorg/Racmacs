0d69ace7cebc79453e0667fb5c9fae286dd7f3fc
Sam Wilks <sw463@cam.ac.uk>
2021-06-22 15:00:21 +0100

    Document and export the mapGadget function

--- R/shiny_gadget.R
+++ R/shiny_gadget.R
@@ -1,4 +1,13 @@
 
+#' Open a shiny gadget to view the map
+#'
+#' This function is equivalent to running `runGUI()` and loading a map file, but this
+#' takes the acmap object to open as an input argument.
+#'
+#' @param map The acmap object to open in the GUI
+#'
+#' @export
+#'
 mapGadget <- function(map) {
 
   ui <- mapGUI_ui()

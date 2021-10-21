ccdf2b262d136bd6ca700c5854bdd109cdb47011
Sam Wilks <sw463@cam.ac.uk>
2021-10-21 12:01:11 +0100

    Fix error where point hovers could stack up

--- inst/htmlwidgets/RacViewer/lib/functions/selections.js
+++ inst/htmlwidgets/RacViewer/lib/functions/selections.js
@@ -160,7 +160,7 @@ Racmacs.Point.prototype.click = function(e){
 // Set prototypes
 Racmacs.Point.prototype.hover = function(){
     
-    if(this.shown){
+    if(this.shown && !this.hovered){
 
         this.hovered = true;
         
@@ -189,27 +189,30 @@ Racmacs.Point.prototype.hover = function(){
 
 Racmacs.Point.prototype.dehover = function(){
     
-    this.hovered = false;
+    if (this.hovered) {
 
-    // Hide the fill for transparent objects
-    if(this.fillColor == "transparent"){
-        if(this.element){
-            this.element.setFillColor("#ffffff");
-            this.element.setFillOpacity(0);
+        this.hovered = false;
+
+        // Hide the fill for transparent objects
+        if(this.fillColor == "transparent"){
+            if(this.element){
+                this.element.setFillColor("#ffffff");
+                this.element.setFillOpacity(0);
+            }
         }
+
+        this.updateDisplay();
+
+        // Show the point info
+        this.moveFromTop();
+        this.hideInfo();
+
+        // Run additional dehover functions
+        this.viewer.dispatchEvent("point-dehovered", { 
+            point : this 
+        });
+
     }
-
-    this.updateDisplay();
-
-    // Show the point info
-    this.moveFromTop();
-    this.hideInfo();
-
-    // Run additional dehover functions
-    this.viewer.dispatchEvent("point-dehovered", { 
-        point : this 
-    });
-
 
 }
 

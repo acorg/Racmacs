f8b98f54123e36dbed7a080080dc98e9f95ca331
Sam Wilks <sw463@cam.ac.uk>
2022-12-29 13:36:40 +0000

    Do not export test viewers by default

--- R/utils_tests.R
+++ R/utils_tests.R
@@ -2,14 +2,7 @@
 # Function to export a plot to the testoutput folder
 export.plot.test <- function(code, filename, plotwidth = 8, plotheight = 8) {
 
-  rootdir <- testthat::test_path("../testoutput/plots")
-  testfile <- file.path(normalizePath(rootdir), filename)
-
-  grDevices::pdf(testfile, plotwidth, plotheight)
-  utils::capture.output(print(code))
-  grDevices::dev.off()
-
-  testthat::expect_true(file.exists(testfile))
+  testthat::expect_true(1 == 1)
 
 }
 
@@ -17,17 +10,7 @@ export.plot.test <- function(code, filename, plotwidth = 8, plotheight = 8) {
 # Function to export a plotly widget to a test page
 export.plotly.test <- function(widget, filename) {
 
-  rootdir <- testthat::test_path("../testoutput/plots")
-  testfile <- file.path(normalizePath(rootdir), filename)
-
-  htmlwidgets::saveWidget(
-    widget,
-    file          = testfile,
-    selfcontained = FALSE,
-    libdir        = ".lib"
-  )
-
-  testthat::expect_true(file.exists(testfile))
+  testthat::expect_true(inherits(widget, "plotly"))
 
 }
 
@@ -37,40 +20,7 @@ export.plotly.test <- function(widget, filename) {
 # package to help with debugging
 export.viewer.test <- function(widget, filename, widgetname = "RacViewer") {
 
-  rootdir <- testthat::test_path("../testoutput/viewer")
-  testfile <- file.path(normalizePath(rootdir), filename)
-
-  htmlwidgets::saveWidget(
-    widget,
-    file          = testfile,
-    selfcontained = FALSE,
-    libdir        = ".lib"
-  )
-
-  unlink(
-    file.path(rootdir, paste0(".lib/", widgetname, "-1.0.4")),
-    recursive = T
-  )
-
-  plotdata <- readLines(testfile)
-  if (widgetname == "RacViewer") {
-    plotdata <- gsub(
-      pattern     = paste0(".lib/", widgetname, "-1.0.4/"),
-      replacement = paste0("../../../inst/htmlwidgets/", widgetname, "/lib/"),
-      x           = plotdata,
-      fixed       = TRUE
-    )
-  } else {
-    plotdata <- gsub(
-      pattern     = paste0(".lib/", widgetname, "-1.0.4/"),
-      replacement = paste0("../../../inst/htmlwidgets/"),
-      x           = plotdata,
-      fixed       = TRUE
-    )
-  }
-  writeLines(plotdata, testfile)
-
-  # Add a test to check plot was outputted correctly
-  testthat::expect_true(file.exists(testfile))
+  # Expect widget created successfully
+  testthat::expect_true(inherits(widget, "htmlwidget"))
 
 }

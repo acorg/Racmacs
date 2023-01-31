2dd16a7936fe3bcfd75cf7c99f9ce6f245358494
Sam Wilks <sw463@cam.ac.uk>
2023-01-31 18:41:30 +0100

    Correct argument for updated r3js library

--- inst/htmlwidgets/RacViewer/lib/functions/bootstrap.js
+++ inst/htmlwidgets/RacViewer/lib/functions/bootstrap.js
@@ -86,7 +86,7 @@ Racmacs.Point.prototype.addBootstrapPoints = function(data){
             var element = new R3JS.element.Point({
                 coords : coord,
                 size : 0.5,
-                shape : "circle3d",
+                shape : "sphere",
                 properties : {
                     lwd : 1,
                     fillcolor : {
